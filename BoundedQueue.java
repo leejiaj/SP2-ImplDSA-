abf62c652bfd5348cd38d6609f104306e8fb6e5c
Leejia James <leejiaj@gmail.com>
2018-09-04 22:34:24 -0500

    added poll() operation

--- BoundedQueue.java
+++ BoundedQueue.java
@@ -1,5 +1,7 @@
 package PXA172130;
 
+import java.util.Scanner;
+
 public class BoundedQueue<T> {
 	int rear =0;
 	int maxSize = 0;
@@ -24,7 +26,15 @@ public class BoundedQueue<T> {
 	
 	T poll()
 	{
-		return null;
+		if(isEmpty()) {
+			return null;
+		}
+		Object temp = bqueue[0];
+		for(int i=0; i<rear-1; i++) {
+			bqueue[i] = bqueue[i+1];
+		}
+		rear--;
+		return (T) temp;
 	}
 	
 	T peek()
@@ -54,18 +64,45 @@ public class BoundedQueue<T> {
 	
 	void printQueue()
 	{
-		for(Object obj: bqueue)
-		{
-			System.out.println(obj);
+		System.out.print(size() + ": ");
+		for(int i=0; i<rear; i++) {
+			System.out.print(bqueue[i] + " ");
 		}
+
+		System.out.println();
 	}
 	
 	public static void main(String args[])
 	{
-		BoundedQueue<Integer> b1 = new BoundedQueue<>(4);
-		b1.offer(1);
-		b1.offer(2);
-		System.out.println("size:"+b1.size());
+		int n = 5;
+		if(args.length > 0) {
+			n = Integer.parseInt(args[0]);
+		}
+		
+		BoundedQueue<Integer> b1 = new BoundedQueue<>(10);
+		for(int i=1; i<=n; i++) {
+			b1.offer(Integer.valueOf(i));
+		}
+		b1.printQueue();
+		
+		Scanner in = new Scanner(System.in);
+		whileloop:
+		while(in.hasNext()) {
+			int com = in.nextInt();
+			switch(com) {
+			case 1:  // Add a new element x at the rear of the queue
+				int elemAdd = in.nextInt();
+				b1.offer(elemAdd);
+				b1.printQueue();
+				break;
+			case 2:  // Remove and return the element at the front of the queue
+				System.out.println(b1.poll());
+				b1.printQueue();
+				break;
+			default:  // Exit loop
+				break whileloop;
+			}
+		}
 		b1.printQueue();
 	}
 }
